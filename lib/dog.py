5568bcd72424f58fea16d5b44ff1f6fff2ac5466
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-08-09 12:14:39 +0300

    attribtes

--- lib/dog.py
+++ lib/dog.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
-    "Shar Pei",
+    "Shiba Inu",
     "Beagle",
     "French Bulldog",
     "Pug",
@@ -12,4 +10,34 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        self._name = None
+        self._breed = None
+        
+        # Only call the setter if the argument was explicitly provided
+        if name is not None:
+            self.set_name(name)
+        if breed is not None:
+            self.set_breed(breed)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
